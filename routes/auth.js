aed7107b7de92a29d9401931913c97a495b603cc
arkarn04 <karnaryan253@gmail.com>
2021-05-01 20:27:54 +0530

    User authentication done

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const session = require('express-session')
 const User = require('../models/user');
 
@@ -16,16 +17,18 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', async(req, res) => {
-    const { name, email, dateOfBirth, password } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        name,
-        email,
-        dateOfBirth,
-        password: hash
-    })
-    await user.save();
-    req.session.user_id = user._id;
+    const { username, email, dateOfBirth, password } = req.body;
+    const user = new User({ username, email, dateOfBirth });
+    const registeredUser = await User.register(user, password);
+    // const hash = await bcrypt.hash(password, 12);
+    // const user = new User({
+    //     name,
+    //     email,
+    //     dateOfBirth,
+    //     password: hash
+    // })
+    // await user.save();
+    // req.session.user_id = user._id;
     res.redirect('/products');
 })
 
@@ -33,21 +36,34 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 })
 
-router.post('/login', async(req, res) => {
-    const { email, password } = req.body;
-    const foundUser = await User.findOne({ email });
-    const isValidUser = await bcrypt.compare(password, foundUser.password);
-    if (isValidUser) {
-        req.session.user_id = foundUser._id;
-        res.redirect('/products')
-    } else {
-        res.redirect('/user/login')
-    }
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/products',
+    faliureRedirect: '/user/login'
+}), (req, res) => {
+    // req.flash('success', 'Welcome!!!')
+    // res.redirect('/products')
 })
 
-router.post('/logout', (req, res) => {
-    req.session.user_id = null;
+// router.post('/login', async(req, res) => {
+//     const { email, password } = req.body;
+//     const foundUser = await User.findOne({ email });
+//     const isValidUser = await bcrypt.compare(password, foundUser.password);
+//     if (isValidUser) {
+//         req.session.user_id = foundUser._id;
+//         res.redirect('/products')
+//     } else {
+//         res.redirect('/user/login')
+//     }
+// })
+
+router.get('/logout', (req, res) => {
+    req.logout();
     res.redirect('/user/login')
 })
+
+// router.post('/logout', (req, res) => {
+//     req.session.user_id = null;
+//     res.redirect('/user/login')
+// })
 
 module.exports = router
