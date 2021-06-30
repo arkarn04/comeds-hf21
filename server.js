91a98b9bc0b7a90ed29d84053f662dafc7dff802
arkarn04 <karnaryan253@gmail.com>
2021-07-01 00:14:26 +0530

    Correctd severalbugs realtd to twilio

--- server.js
+++ server.js
@@ -56,8 +56,13 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(require('express-session')({
     secret: "thiscouldabettersecret",
-    resave: false,
-    saveUninitialized: false
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -68,7 +73,7 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
